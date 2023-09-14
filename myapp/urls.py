7d2c3c19eb4c78a2614e8b764b744f7615cfaf5f
nail-rinatovich <nailfax1991@yandex.ru>
2023-09-14 22:30:51 +0200

    add robots.txt

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,11 @@ from .views import homePageView
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+from django.views.generic.base import TemplateView 
 urlpatterns = [
     path('', index, name='index'),
+    
+    path('robots.txt',TemplateView.as_view(template_name="main/robots.txt", content_type="text/plain")),  #add the robots.txt file
+
+    
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -13,3 +13,14 @@ def index(request):
     else:
         form = ContactForm()
     return render(request, 'index.html', {'form': form})
+from django.views.decorators.http import require_GET
+
+
+@require_GET
+def robots_txt(request):
+    lines = [
+        "User-Agent: *",
+        "Disallow: /private/",
+        "Disallow: /junk/",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
